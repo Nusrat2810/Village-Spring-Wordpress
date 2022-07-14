4952f43922fb3f7e55f961103e5d7993b8de698c
Nusrat Farzana <nusu2810@gmail.com>
2022-07-14 14:39:40 +0200

    template parts added

---
+++ themes/nusrat/template-parts/connect.php
@@ -0,0 +1,28 @@
+	<section id="stay-up" class="container-fluid px-0">
+	    <div class="section-inner py-5">
+	        <div class="container">
+	            <div class="heading">
+	                <h2 class="heading-title text-white">Stay Up To Date</h2>
+	            </div>
+	            <div class="contact-form d-flex justify-content-center">
+	                <form id="form-newsletter" class="row" action="#" method="POST">
+	                    <div class="col-md-6 form-group my-1">
+	                        <label for="email" class="sr-only">Email</label>
+	                        <input type="email" name="email" class="form-control form-control-lg rounded-0 shadow-none" id="email" placeholder="Email" required>
+	                    </div>
+	                    <div class="col-md-6 my-1">
+	                        <button type="submit" name="btnAction" value="subscribe" class="btn btn-lg my-button w-100">Connect With Us</button>
+	                    </div>
+	                </form>
+	            </div>
+	        </div>
+	    </div>
+	</section>
+
+	<section id="clear" class="container-fluid py-5">
+	    <div class="container text-center">
+	        <div class="logo">
+	            <img src="<?php echo get_theme_file_uri('assets/img/logo-footer.png'); ?>" alt="...">
+	        </div>
+	    </div>
+	</section>
